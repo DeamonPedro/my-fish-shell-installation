bd930756ba362b54db9a678ac9d7ded64926cbef
Pedro Lucas Carneiro <pedro.lucas.root@gmail.com>
2022-07-23 15:22:43 -0300

    fix(json): bin path call fixed

--- functions/json.fish
+++ functions/json.fish
@@ -1,12 +1,12 @@
 function json -d "json short for data-selector" -a 'file' -a 'query'
-    if test -t 0
-        if test -z "$file" -a -z "$query"
-            echo "json: invalid arguments"
-            return
-        end
-        cat "$file" | jfq "$query" -j | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g"
-    else
+    if not test -t 0
         set query $file
-        cat /dev/stdin | jfq "$query" -j | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g"
+        set file "/dev/stdin"
     end
+    if test -z "$file" -a -z "$query"
+        echo "json: invalid arguments"
+        return
+    end
+    set jfq (readlink -f ../bin/jfq)
+    cat "$file" | eval "$__fish_config_dir/bin/jfq '$query' -j" | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g"
 end
